13bf5192dfa8406ee04e4ed269f949ffa38fd89f
Pedro-Mamud <pedro.mamud@cnpem.br>
2025-02-13 13:28:35 -0300

    commit just for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ESP32httpUpdate.h>
+#include <Preferences.h>
 
 #define WIFI_SSID "Devices"
 #define WIFI_PASSWORD ""  
+
+Preferences preferences;
+
+uint8_t currentVersion = 2;
+uint8_t lastVersion = 0;
 
 void connectWiFi() {   
     Serial.println("Iniciando conexão WiFi...");
@@ -41,6 +47,10 @@ void updateOTA() {
 
     case HTTP_UPDATE_OK:
         Serial.println("HTTP_UPDATE_OK");
+        preferences.begin("firmware", false);
+        preferences.putUInt("version", currentVersion);
+        preferences.end();
+        ESP.restart();  // Reinicia para aplicar a nova versão
         break;
     }
 }
@@ -48,6 +58,11 @@ void updateOTA() {
 // Checagem de atualização
 void checkUpdate() {
 
+    preferences.begin("firmware", false);
+    lastVersion = preferences.getUInt("version", 0);
+    preferences.end();
+
+    if (currentVersion != lastVersion)
         Serial.println("Inicializando HTTP para checagem de atualização...");
         delay(500);
         HTTPClient http;
@@ -55,14 +70,14 @@ void checkUpdate() {
         http.setConnectTimeout(4000);
         http.setTimeout(4000);
         int resCode = http.GET();
-        if (resCode > 0) {
+        if (resCode > 0) {    
             updateOTA();
         }
 }
 
 void setup() {
     Serial.begin(9600);
-    delay(2000);
+    delay(2000);  
     connectWiFi();
 }
 
